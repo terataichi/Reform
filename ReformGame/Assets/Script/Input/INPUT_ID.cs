662023afaf556475194d3bb66490325f81763017
HiH-0906 <homuihomu0906@gmail.com>
2020-10-01 01:19:40 +0900

    ねむねむ

---
+++ ReformGame/Assets/Script/Input/INPUT_ID.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum INPUT_ID
+{
+    // 入力関係アクセス用　現状特にアクションが決まっていないため見た目で名付けた感じ
+    MARU,           // 丸
+    BATU,           // バツ
+    SANKAKU,        // 四角
+    SIKAKU,         // 三角
+    MAX
+}
---
+++ ReformGame/Assets/Script/Input/InputState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InputState : MonoBehaviour
+{
+    protected Dictionary<(bool now, bool old),INPUT_ID> inputList_ = new Dictionary<(bool,bool), INPUT_ID>();     // INPUT_IDに対応した、ボタン押下格納先
+    protected (Vector2 now, Vector2 old) analogInputL_;              // 左スティックの入力状態  
+    protected (Vector2 now, Vector2 old) analogInputR_;              // 右スティックの入力状態 
+
+    public InputState()
+    {
+        for()
+        {
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
