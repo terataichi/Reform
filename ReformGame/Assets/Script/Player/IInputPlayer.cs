b3ff2986dd90549165156cf4713174546c364ba7
terataichi <taichiz519@gmail.com>
2020-10-02 17:43:48 +0900

    kommit

---
+++ ReformGame/Assets/Script/Player/IInputPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//namespace Player
+//{
+    // プレイヤーの入力を渡すためのインターフェース
+    public interface IInputPlayer
+    {
+        void InputPlayer(Input input);
+    }
+//}
