06cbad4e72457b4cbc320063c2b1b2d10b931a70
terataichi <taichiz519@gmail.com>
2020-10-05 17:34:41 +0900

    申し訳ございませんでした

---
+++ ReformGame/Assets/Script/Player/IHitPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//namespace Player
+//{
+    public interface IHitPlayer
+    {
+        void HitPlayer(GameObject player);
+    }
+//}
---
+++ ReformGame/Assets/Script/Player/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameInput;
+
+namespace Player
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private InputState input_ = null;
+        [SerializeField] private PlayerMover mover_ = null;
+        [SerializeField] private GameObject mainCamera_ = null;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            input_.InputUpdate();
+            mover_.MoveUpdate(input_, mainCamera_);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            // 当たった対象のにIHitPlayerがあったら呼ぶ
+            var hitPlayer = other.gameObject.GetComponent<IHitPlayer>();
+            if(hitPlayer != null)
+            {
+                hitPlayer.HitPlayer(this.gameObject);
+            }
+        }
+    }
+}
