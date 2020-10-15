ba894742191f7ed75e4f6ea170c4975fcf7eb8b9
HiH-0906 <homuihomu0906@gmail.com>
2020-10-15 18:12:29 +0900

    ﾖｼｯ

---
+++ ReformGame/Assets/Script/State/PLAYERSTATE.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public enum PLAYERSTATE
+    {
+        STAY,           // 待機
+        RUN,            // 走り
+        HOLD,           // 持つ
+        PULL,           // 引く
+        INQUIRE,        // 引き合う
+        PUSH            // 押す
+    }
+}
